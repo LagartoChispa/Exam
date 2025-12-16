fa0287e769890dd9a60fe34ef68070427d536d97
TuNombre <TuEmail@ejemplo.com>
2025-12-15 21:54:54 -0300

    feat: Update backend URL and Gradle plugin version
        Updates the base URL for the API to point to the new backend service.
        Also, updates the Android Gradle Plugin to version 8.13.2.

--- app/src/main/java/com/exam/me/network/RetrofitInstance.kt
+++ app/src/main/java/com/exam/me/network/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://10.0.2.2:3010/api/"
+    private const val BASE_URL = "https://cineplus-api-ezxk.onrender.com/api/"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
